2f3345ee42c6327234ade12ceb530a26c871c4e2
Shayi <szhang@peachtreebilling.com>
2014-05-23 10:06:45 -0400

    Add gitconfig and install script

---
+++ install.rb
@@ -0,0 +1,15 @@
+files = [
+  '.gitconfig',
+  '.gitmessage'
+]
+
+files.each do |file|
+  target_file = File.expand_path("~/#{ file }")
+  source_file = File.expand_path("~/.dotfiles/#{ file }")
+
+  puts "skipping #{ target_file }" if File.exists?(target_file)
+
+  unless File.exists?(target_file) || File.symlink?(target_file)
+    File.symlink(source_file, target_file)
+  end
+end
