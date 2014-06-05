66a56666232d6325870cebdd9a1c2fcb556caa03
Shayi <szhang@peachtreebilling.com>
2014-06-05 17:58:31 -0400

    sublime settings

--- install.rb
+++ install.rb
@@ -13,3 +13,13 @@ files.each do |file|
     File.symlink(source_file, target_file)
   end
 end
+
+# sublime text
+target_file = File.expand_path("~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings")
+source_file = File.expand_path("~/.dotfiles/sublime/Preferences.sublime-settings")
+
+if File.exists?(target_file) && ! File.symlink?(target_file)
+  File.delete(target_file)
+else
+  File.symlink(source_file, target_file)
+end
